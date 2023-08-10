bfde8373f54e8645590ea036fe368fab07580337
Alexgao001 <alex.g@nodereal.io>
2023-08-10 14:42:34 +0800

    change parameter

--- listener/const.go
+++ listener/const.go
@@ -3,6 +3,6 @@ package listener
 import "time"
 
 const (
-	NumOfHistoricalBlocks = int64(100000) // NumOfHistoricalBlocks is the number of blocks will be kept in DB, all transactions and votes also kept within this range
+	NumOfHistoricalBlocks = int64(10000) // NumOfHistoricalBlocks is the number of blocks will be kept in DB, all transactions and votes also kept within this range
 	PurgeJobInterval      = time.Minute * 2
 )
