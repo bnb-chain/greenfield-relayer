126d79b8dee6be2175ff695f93edb7adb8619e67
Alexgao001 <alex.g@nodereal.io>
2023-08-15 14:55:37 +0800

    fix import

--- listener/const.go
+++ listener/const.go
@@ -1,6 +1,8 @@
 package listener
 
-import "time"
+import (
+	"time"
+)
 
 const (
 	NumOfHistoricalBlocks = int64(10000) // NumOfHistoricalBlocks is the number of blocks will be kept in DB, all transactions and votes also kept within this range
